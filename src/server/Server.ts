3b6ea04fb4afe6150a417d636e9613ad4629a937
math_magno <matheusmagno11@gmail.com>
2026-04-17 14:41:25 -0300

    feat

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
+import { router } from './routes'
+
 
 const server = express();
 
+server.use(express.json());
 
-server.get('/', (_, res) => {
-  return res.send(`Olá, DEV!`)
-});
-
-
-
+server.use(router);
 
 
 
--- src/server/index.ts
+++
@@ -1,3 +0,0 @@
-import { server } from './Server';
-
-server.listen(3333, () => console.log('App rodando!'))
